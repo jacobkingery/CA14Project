f302eb2c5c619f648c97fb80039d5a25c747cacd
Jacob Kingery <jacob.kingery@students.olin.edu>
2014-12-08 00:36:20 -0500

    square wave generator that synthesizes

--- doSquareWave.do
+++ doSquareWave.do
@@ -2,10 +2,13 @@ vdel -all
 vlib work
 vmap work work
 vlog -reportprogress 300 -work work squareWave.v
-vsim -voptargs="+acc" testFreqDiv
+vsim -voptargs="+acc" testSquareWave
 add wave -position insertpoint \
-sim:/testFreqDiv/clk \
-sim:/testFreqDiv/DUT/counter \
-sim:/testFreqDiv/out 
-run 100000
+sim:/testSquareWave/clk \
+sim:/testSquareWave/outG \
+sim:/testSquareWave/outA \
+sim:/testSquareWave/outB \
+sim:/testSquareWave/outC \
+sim:/testSquareWave/outD 
+run 5000000
 wave zoom full
--- squareWave.v
+++ squareWave.v
@@ -1,31 +1,69 @@
-module freqDiv (clk, out);
-parameter clkDivider = 50000000/440/2;
+module squareWave (clk, out);
+    parameter desiredFreq = 440;
+    parameter clkFreq = 50000000;
+    // To get desired frequency out, we need to divide the clock
+    // frequency.  However, because we will not always be dividing
+    // by a power of 2, we can create a counter that counts up 
+    // twice as fast as desired and toggle the output whenever
+    // the counter reaches its maximum to get a 50% duty cycle
+    // square wave at the desired frequency.
+    parameter clkDivider = clkFreq/desiredFreq/2;
 
-input clk;
-output reg out;
-initial out = 0;
+    input clk;
+    output reg out;
+    initial out = 0;
 
-// Oversizing because why not
-reg[31:0] counter;
-initial counter = 0;
+    // Oversizing because why not
+    reg[31:0] counter;
+    initial counter = 0;
 
-always @(posedge clk) begin
-    if (counter == clkDivider-1) begin
-        counter <= 0;
-        out <= ~out;
-    end else begin
-        counter <= counter + 1;
+    always @(posedge clk) begin
+        if (counter == clkDivider-1) begin
+            counter <= 0;
+            out <= ~out;
+        end else begin
+            counter <= counter + 1;
+        end
     end
-end
 endmodule
 
-module testFreqDiv;
-reg clk;
-initial clk = 0;
-always #5 clk = ~clk;
 
-wire out;
+// module testSquareWave;
+//     reg clk;
+//     initial clk = 0;
+//     always #10 clk = ~clk;
 
-freqDiv DUT (clk, out);
+//     wire outG;
+//     squareWave #(392) G4 (clk, outG);
+//     wire outA;
+//     squareWave #(440) A4 (clk, outA);
+//     wire outB;
+//     squareWave #(494) B4 (clk, outB);
+//     wire outC;
+//     squareWave #(523) C5 (clk, outC);
+//     wire outD;
+//     squareWave #(587) D5 (clk, outD);
+// endmodule
+
+module squareWaveBringup(clk, sw, gpioBank1);
+    input clk;
+    input[7:0] sw;
+    output[3:0] gpioBank1;
+
+    wire outG;
+    squareWave #(392) G4 (clk, outG);
+    assign gpioBank1[0] = outG & sw[0];
+
+    wire outA;
+    squareWave #(440) A4 (clk, outA);
+    assign gpioBank1[1] = outA & sw[1];
+
+    wire outB;
+    squareWave #(494) B4 (clk, outB);
+    assign gpioBank1[2] = outB & sw[2];
+
+    wire outC;
+    squareWave #(523) C4 (clk, outC);
+    assign gpioBank1[3] = outC & sw[3];
 
 endmodule
